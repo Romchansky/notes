e9a02faeb835890c5cc0696ebdc3a6c73a838602
romchansky <romkapetrenko8@gmail.com>
2021-11-10 23:03:14 +0200

    create DbInitConfig, initial security config

---
+++ notes/src/main/java/com/goit/notes/configuration/DbInitConfig.java
@@ -0,0 +1,30 @@
+package com.goit.notes.configuration;
+
+import com.goit.notes.entity.Role;
+import com.goit.notes.entity.User;
+import com.goit.notes.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+@Configuration
+public class DbInitConfig {
+
+    private final UserRepository repository;
+
+    @Autowired
+    public DbInitConfig(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @EventListener
+    public void onApplicationEvent() {
+
+        User user = new User();
+        user.setUserName("Admin");
+        user.setPassword("$2a$10$NerjWhAeGwlyeDiJ4rl2nuf.4aM4BFSN3Ll5cinNzvBHREOKfTSVG"); //12345
+        user.setUserRole(Role.ROLE_ADMIN);
+        repository.save(user);
+        repository.flush();
+    }
+}
---
+++ notes/src/main/resources/db/migration/V2_addData.sql
@@ -0,0 +1,2 @@
+insert into users
+VALUES (default, 'admin', '$2a$10$f3LJBLWEo9IlknoUJkTunOQUsYII8IfQI2FcTchAvRk9ljkb9TYNe', 'ROLE_ADMIN');
