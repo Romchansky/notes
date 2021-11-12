a93be7e17f23cdedb72d31396f5e543ca7b88c80
alice_just <alicejust53@gmail.com>
2021-11-12 13:55:58 +0200

    update mainController

--- notes/src/main/java/com/goit/notes/controller/MainController.java
+++ notes/src/main/java/com/goit/notes/controller/MainController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,20 +14,23 @@ public class MainController {
 
     @GetMapping
     public String doGet() {
-        return "redirect:/note/list";
+        return "index";
     }
-    
+
+    @PostMapping
+    public String doPost() {
+        return "index";
+    }
 
     @GetMapping("login")
     public String login(Model model, String error, String logout) {
         if (error != null) {
-            model.addAttribute("error", "You login or password not corrected, check it " +
-                    "and try again");
+           log.info ("You login or password not corrected, check it " +
+                        "and try again");
         }
         if (logout != null) {
-            model.addAttribute("message", "You have been logged out");
+                log.info ("You have been logged out");
         }
-        return "login";
+            return "login";
+        }
     }
-
-}
