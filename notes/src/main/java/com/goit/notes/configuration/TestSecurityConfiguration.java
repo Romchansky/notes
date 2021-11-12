1a147cfb278b5b2c4793381a49ed5a5ea620dc79
alice_just <alicejust53@gmail.com>
2021-11-12 13:53:43 +0200

    add testing security

---
+++ notes/src/main/java/com/goit/notes/configuration/TestSecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.goit.notes.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class TestSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    private final UserDetailsServiceImpl customUserDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(customUserDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().antMatchers("/listAllUsers").access("hasRole('ROLE_ADMIN')");
+        http.csrf ().disable ()
+                .authorizeRequests ()
+                .antMatchers ("/user/registration").permitAll ()
+                .anyRequest ().authenticated ()
+                .and ()
+                .formLogin ()
+                .loginPage ("/login")
+                .defaultSuccessUrl ("/")
+                .permitAll ()
+                .and ()
+                .logout ()
+                .permitAll ();
+    }
+
+
+}
