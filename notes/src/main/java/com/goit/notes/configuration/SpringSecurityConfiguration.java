defac820fe03c51f5cad10963aa3f0095bf4f80a
alice_just <alicejust53@gmail.com>
2021-11-13 08:13:54 +0200

    add login page

--- notes/src/main/java/com/goit/notes/configuration/SpringSecurityConfiguration.java
+++ notes/src/main/java/com/goit/notes/configuration/SpringSecurityConfiguration.java
@@ -1,63 +1,63 @@
 package com.goit.notes.configuration;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-@RequiredArgsConstructor
-@Configuration
-@EnableWebSecurity
-public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //
-//    private final UserDetailsServiceImpl customUserDetailsService;
+//@RequiredArgsConstructor
+//@Configuration
+//@EnableWebSecurity
+//public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 //
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+////
+////    private final UserDetailsServiceImpl customUserDetailsService;
+////
+////    @Bean
+////    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+////        return new BCryptPasswordEncoder();
+////    }
+////
+////
+////    @Autowired
+////    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+////        authenticationManagerBuilder.userDetailsService(customUserDetailsService)
+////                .passwordEncoder(new BCryptPasswordEncoder());
+////    }
+////
+////
+////    @Override
+////    protected void configure(HttpSecurity http) throws Exception {
+////
+////        http.csrf().disable();
+////
+////        //Страница /userInfo требует входа в систему как ROLE_USER
+////        http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER')");
+////
+////        // Только для роли Admin
+////        http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
+////
+////        // Если нет прав (роли) для доступа, будет перенаправлен на страницу регистрации
+////
+////
+////        //login config
+////        http.authorizeRequests().and().formLogin()
+////                .loginProcessingUrl("/") // submit URL
+////                .loginPage("/login")
+////                .defaultSuccessUrl("/userInfo")
+////                .failureUrl("/login?error=true")
+////                .usernameParameter("username")
+////                .passwordParameter("password")
+////                // logout config
+////                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/logoutSuccessful");
+////
+////
+////    }
 //
 //
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-//        authenticationManagerBuilder.userDetailsService(customUserDetailsService)
-//                .passwordEncoder(new BCryptPasswordEncoder());
-//    }
-//
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//        http.csrf().disable();
-//
-//        //Страница /userInfo требует входа в систему как ROLE_USER
-//        http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER')");
-//
-//        // Только для роли Admin
-//        http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
-//
-//        // Если нет прав (роли) для доступа, будет перенаправлен на страницу регистрации
-//
-//
-//        //login config
-//        http.authorizeRequests().and().formLogin()
-//                .loginProcessingUrl("/") // submit URL
-//                .loginPage("/login")
-//                .defaultSuccessUrl("/userInfo")
-//                .failureUrl("/login?error=true")
-//                .usernameParameter("username")
-//                .passwordParameter("password")
-//                // logout config
-//                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/logoutSuccessful");
-//
-//
-//    }
-
-
-}
+//}
