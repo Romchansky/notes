467054306e43b50ba20f620a4d890f6f525ce6d5
Алиса Кондратенко <78209371+Alice-pinguin@users.noreply.github.com>
2021-11-14 07:55:03 +0200

    Update NoteService.java

--- notes/src/main/java/com/goit/notes/service/NoteService.java
+++ notes/src/main/java/com/goit/notes/service/NoteService.java
@@ -17,32 +17,35 @@ import java.util.UUID;
 @Slf4j
 public class NoteService extends BaseService<Note, UUID> {
 
-    private NoteRepository repository;
+    private final NoteRepository repository;
 
-    public NoteService(NoteRepository repository) {
+    public NoteService(NoteRepository repository){
         super(repository);
+        this.repository = repository;
+
     }
 
     public Note createNote(Note note) {
+
         return repository.save(note);
     }
-
-    public Note shareNote(UUID id) {
+    public Note shareNote(UUID id){
         if (repository.existsById(id)) {
             Note note = repository.getById(id);
             if (note.getAccess().equals(Access.PUBLIC)) {
                 return note;
             } else {
-                throw new RuntimeException("Note is private");
+                    throw new RuntimeException("Note is private");
             }
         }
-        throw new RuntimeException("note with such id does`t exists");
-    }
+            throw new RuntimeException("note with such id doesn`t exists");
+        }
 
-    public Note editNote(Note note) {
-        return createNote(note);
+        public Note editNote(Note note) {
+            return createNote(note);
+        }
+
     }
-}
 /*
  * проверка на ограничение длины заметки
  * поделиться заметкой должна вернуться ссылка на неё
