4be36cefa27bf0cbbca1e8df0021b2074c1c2c8a
alice_just <alicejust53@gmail.com>
2021-11-14 15:23:54 +0200

    update service

--- notes/src/main/java/com/goit/notes/repository/NoteRepository.java
+++ notes/src/main/java/com/goit/notes/repository/NoteRepository.java
@@ -5,8 +5,11 @@ import com.goit.notes.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, UUID> {
+
+    Optional<Note> findByName(String name);
 }
