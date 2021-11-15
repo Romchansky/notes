a3ad56fb7084c825e2306f7c808c39ffba6d986b
alice_just <alicejust53@gmail.com>
2021-11-15 14:51:26 +0200

    update service

--- notes/src/main/java/com/goit/notes/controller/MainController.java
+++ notes/src/main/java/com/goit/notes/controller/MainController.java
@@ -3,7 +3,6 @@ package com.goit.notes.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,7 +29,7 @@ public class MainController {
     public String login(String error, String logout, Model model) {
 
         model.addAttribute("title", "Login");
-        model.addAttribute("message", "Login or create new account");
+        model.addAttribute("message", "To start using login or create an account");
 
         if (error != null) log.info("You login or password not corrected, check it and try again");
         if (logout != null) log.info("You have been logged out");
