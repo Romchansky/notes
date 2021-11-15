4e33c5f0baa20b7ea9ce03139dcd796141a43816
alice_just <alicejust53@gmail.com>
2021-11-15 15:25:08 +0200

    update usercontroller

--- notes/src/main/java/com/goit/notes/controller/UserController.java
+++ notes/src/main/java/com/goit/notes/controller/UserController.java
@@ -3,6 +3,7 @@ package com.goit.notes.controller;
 import com.goit.notes.entity.User;
 import com.goit.notes.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping(path = "/user")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -28,7 +29,12 @@ public class UserController {
 
     @PostMapping("/register")
     public String registrationUser(@Valid User user) {
+        if(user.getPassword ().length ()<8)
+            log.info ("Password length must be from 8 to 100 symbols");
+        if(user.getUserName ().length ()<5 )
+            log.info ("Username length must be from 5 to 50 symbols");
         userService.register (user);
+
         return "redirect:/login";
 
     }
