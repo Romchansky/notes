7e3e0221a63ab5d854087e849f97fbebdd060d2f
alice_just <alicejust53@gmail.com>
2021-11-16 12:58:30 +0200

    update usercontroller

--- notes/src/main/java/com/goit/notes/controller/UserController.java
+++ notes/src/main/java/com/goit/notes/controller/UserController.java
@@ -3,9 +3,12 @@ package com.goit.notes.controller;
 import com.goit.notes.entity.User;
 import com.goit.notes.service.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,37 +16,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import javax.validation.Valid;
 
+
 @RequiredArgsConstructor
 @Controller
 @RequestMapping(path = "/user")
-@Slf4j
+
 public class UserController {
+    private static final Log log = LogFactory.getLog (UserController.class);
 
     private final UserService userService;
 
     @GetMapping("/register")
-    public String registration() {
+    public String registration(Model model) {
+        model.addAttribute("title", "Registration");
         return "register";
     }
 
-
     @PostMapping("/register")
-    public String registrationUser(@Valid User user) {
-        if(user.getPassword ().length ()<8)
-            log.info ("Password length must be from 8 to 100 symbols");
-        if(user.getUserName ().length ()<5 )
-            log.info ("Username length must be from 5 to 50 symbols");
-        userService.register (user);
+    public String registrationUser(@Valid User user, BindingResult result)
+    {
+        if(result.hasErrors ()){
+            return "register";
+        }
 
+        userService.register(user);
         return "redirect:/login";
-
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @GetMapping(path = "/listUsers")
+    @GetMapping("/listUsers")
     public ModelAndView showAllUsersPage(ModelAndView model) {
-        model.addObject ("users", userService.findAll ());
-        model.setViewName ("listUsers");
+        model.addObject("users", userService.findAll());
+        model.setViewName("listUsers");
         return model;
     }
 
@@ -52,3 +56,4 @@ public class UserController {
         return new User();
     }
 }
+
