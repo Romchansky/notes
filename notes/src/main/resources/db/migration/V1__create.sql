0ed44fe0250aa11b79d39f6e7782f5e31a9cce86
Your Name <you@example.com>
2021-11-17 20:05:26 +0200

    fix gradle

--- notes/src/main/resources/db/migration/V1__create.sql
+++ notes/src/main/resources/db/migration/V1__create.sql
@@ -1,18 +1,14 @@
-CREATE TABLE users
-(
-    id  varchar(255) PRIMARY KEY NOT NULL,
-    user_name      varchar(50) UNIQUE NOT NULL,
-    password varchar(100)        NOT NULL,
-    user_role     varchar(10)         NOT NULL
-
-
+CREATE TABLE users(
+    id varchar(255) PRIMARY KEY NOT NULL,
+    user_name varchar(50) UNIQUE NOT NULL,
+    password varchar(100) NOT NULL,
+    user_role varchar(10) NOT NULL
 );
 
-CREATE TABLE note
-(
+CREATE TABLE note(
     id varchar(255) PRIMARY KEY NOT NULL,
-    name        varchar(100)   NOT NULL,
+    name varchar(100) NOT NULL,
     description varchar(10000) NOT NULL,
-    access      varchar(10)    NOT NULL,
-    user_id     varchar(255)   NOT NULL
+    access varchar(10) NOT NULL,
+    user_id varchar(255) NOT NULL
 );
