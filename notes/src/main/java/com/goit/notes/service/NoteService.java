a12b41ce5bfb4792d0243e51fcc4b593b4dc42d7
ADM-Sasha-Sasha <webi9876@gmail.com>
2021-11-17 16:48:28 +0200

    test commit

--- notes/src/main/java/com/goit/notes/service/NoteService.java
+++ notes/src/main/java/com/goit/notes/service/NoteService.java
@@ -1,6 +1,5 @@
 package com.goit.notes.service;
 
-import com.goit.notes.entity.Access;
 import com.goit.notes.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
